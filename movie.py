e249dc3e210441d861f9129468270a618766fc49
kanikabisht0201 <141163071+kanikabisht0201@users.noreply.github.com>
2024-10-31 00:13:11 -0700

    Update movie.py

--- movie.py
+++ movie.py
@@ -6,7 +6,13 @@ import streamlit as st
 def download_file_from_google_drive(file_id):
     url = f'https://drive.google.com/uc?id={file_id}'
     response = requests.get(url)
-    return pickle.loads(response.content)
+    if response.status_code == 200:
+        try:
+            return pickle.loads(response.content)
+        except pickle.UnpicklingError:
+            raise Exception("The downloaded content is not a valid pickle file.")
+    else:
+        raise Exception(f"Failed to download file: HTTP Status Code {response.status_code}")
 
 # Use the actual file IDs from Google Drive
 movie_list_id = '1Fh33IwObUb3vwvuIMQTeY8oEtgTyUNZD'  # Movie list file ID
@@ -22,8 +28,7 @@ except Exception as e:
 
 # Function to fetch poster image from TMDb API
 def fetch_poster(movie_id):
-    # Correctly format the URL for fetching the poster image
-    url = f"https://api.themoviedb.org/3/movie/{movie_id}?api_key=31dea30241045ff2b6e028a1ed1bf151&language=en-US"
+    url = f"https://api.themoviedb.org/3/movie/{movie_id}?api_key=YOUR_API_KEY&language=en-US"
     data = requests.get(url)
     data = data.json()
     poster_path = data.get('poster_path')
